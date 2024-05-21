a06774e132a9f2951f2db70d3bec2e774766663a
HugoMany <90179566+HugoMany@users.noreply.github.com>
2024-05-21 14:59:18 +0200

    Create achat.jsx

---
+++ danceclubjunia/src/PAGES/achat.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+function Achat() {
+  return (
+    <div>
+      <h1>Acheter des titres de cours de danse</h1>
+      <h2>Choisissez votre cours</h2>
+      <div>
+        <h3>Rock</h3>
+        <p>Le rock est une danse très populaire en France. Elle se danse en couple et est très simple à apprendre.</p>
+        <p>Le rock est une danse très populaire en France. Elle se danse en couple et est très simple à apprendre.</p>
+        <p>Le rock est une danse très populaire en France. Elle se danse en couple et est très simple à apprendre.</p>
+        <p>Le rock est une danse très populaire en France. Elle se danse en couple et est très simple à apprendre.</p>
+        <button>Acheter</button>
+      </div>
+    </div>
+  );
+};
+
+export default Achat;
