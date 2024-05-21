e8251af8bc3e54ce9b404f25f84ef3bfc5e84eec
EliottPeres <eliott.peres@student.junia.com>
2024-05-21 15:18:27 +0200

    main

---
+++ danceclubjunia/src/PAGES/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h1>Dance Club</h1>
+            <button style={{ position: 'absolute', top: '10px', right: '10px' }}>Connexion</button>
+        </div>
+    );
+};
+
+export default Main;
