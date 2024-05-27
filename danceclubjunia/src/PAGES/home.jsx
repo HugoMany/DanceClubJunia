b0a0361e500156caf081437f6d6b6da65166a352
HugoMany <90179566+HugoMany@users.noreply.github.com>
2024-05-27 12:26:32 +0200

    Modal

--- danceclubjunia/src/PAGES/home.jsx
+++ danceclubjunia/src/PAGES/home.jsx
@@ -31,8 +31,8 @@ function Home() {
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            width: 400,
-            bgcolor: 'background.paper',
+            width: '80vw',
+            bgcolor: '#0000009e',
             border: '2px solid #000',
             boxShadow: 24,
             p: 4
