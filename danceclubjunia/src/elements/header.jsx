3593a0d85df9b62370906189709bae7ed5233c1d
HugoMany <90179566+HugoMany@users.noreply.github.com>
2024-05-30 11:06:53 +0200

    Style again

--- danceclubjunia/src/elements/header.jsx
+++ danceclubjunia/src/elements/header.jsx
@@ -1,4 +1,3 @@
-import danceImage from '../img/dance.png';
 
 function Header() {
     const isAdmin = false;
@@ -6,18 +5,22 @@ function Header() {
 
 
     return (
+        <div>
         <div class="header">
+            <h1>Danse Club</h1>
+        </div>
+        <div class="footer">
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-            <a href='/'><button><span class="material-symbols-outlined">
+            <div><a href='/'><button><span class="material-symbols-outlined">
             home
-            </span></button></a>
+            </span></button></a></div>
 
-          <h1>Dance Club </h1>
+          <h1>title</h1>
 <br/>       
         {isAdmin ? (
                 <>
                 {/* <h4>isAdmin</h4> */}
-                                <a href='/admin/' ><button class="styled-button">A</button></a>
+                <div><a href='/admin/' ><button class="styled-button">A</button></a></div>
                 </>
 
             ) : (<>
@@ -27,22 +30,24 @@ function Header() {
             {isConnect ? (
                 <>
                 {/* <h4>isConnect</h4> */}
+                <div>
                 <a href='/profil/'><button><span class="material-symbols-outlined">
 person
-</span></button></a>
+</span></button></a></div>
                 </>
 
             ) : (<>
             {/* <h4>isNotConnect</h4> */}
+            <div>
                 <a href='/connexion/'><button><span class="material-symbols-outlined">
 login
-</span></button></a>
+</span></button></a></div>
                 </>
             )}
-<br/><br/>
 
  
 
+        </div>
         </div>
     );
 
---
+++ danceclubjunia/src/elements/listeFuturCours.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CoursesList from '../PAGES/listCourse';
+
+function ListeFuturCours() {
+  return (
+    <div className='listFuturCours'>
+        <CoursesList></CoursesList>
+    </div>
+  );
+}
+
+export default ListeFuturCours;
