7f028982988e8ffe95b430b8c6d131eb27ada2bd
HugoMany <90179566+HugoMany@users.noreply.github.com>
2024-06-03 10:24:56 +0200

    Login again

--- danceclubjunia/src/PAGES/connexion.jsx
+++ danceclubjunia/src/PAGES/connexion.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import Header from '../elements/header';
 import '../css/login.css';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 function Connexion() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -42,26 +52,69 @@ function Connexion() {
     return (
         <div>
             <Header title="Login"></Header>
-            <div className='Connexion'>
-            <h1>Page de Connexion</h1>
+        <div>    
+        
+         <Swiper
+         pagination={{
+             dynamicBullets: true,
+         }}
+         modules={[Pagination]}
+         className="mySwiper"
+     >
+         <SwiperSlide>
+         <div className='Connexion'>
+            {/* <h1>Page de Connexion</h1> */}
             <form onSubmit={handleSubmit}>
                 <label>
-                    E-mail:
-                    <input type="email" value={email} onChange={handleEmailChange} />
+                    <input  placeholder="E-mail" type="email" value={email} onChange={handleEmailChange} />
                 </label>
                 <br />
                 <label>
-                    Mot de passe:
-                    <input type="password" value={password} onChange={handlePasswordChange} />
+                    <label>
+                        
+                        <input  placeholder="Mot de passe" type="password" value={password} onChange={handlePasswordChange} />
+                    </label>
                 </label>
                 <br />
-                <button type="submit">Se connecter</button>
+                <button className='connexionLogin' type="submit">Se connecter</button>
 
                 
             </form>
-            <a href='/inscription/'>Inscription</a>
+            <button className='inscriptionLogin'><a href='/inscription/'> 
+            <span class="material-symbols-outlined">
+swipe_right
+</span> Inscription
+            </a></button>
             </div>
-        </div>
+         </SwiperSlide>
+         <SwiperSlide>
+         <div className='Connexion'>
+            {/* <h1>Page de Connexion</h1> */}
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <input  placeholder="E-mail" type="email" value={email} onChange={handleEmailChange} />
+                </label>
+                <br />
+                <label>
+                    <label>
+                        <input  placeholder="Mot de passe" type="password" value={password} onChange={handlePasswordChange} />
+                    </label>
+                </label>
+                <br />
+                <button className='connexionLogin' type="submit">Se connecter</button>
+
+                
+            </form>
+            {/* <button className='inscriptionLogin'><a href='/inscription/'>Inscription</a></button> */}
+            </div>
+             
+
+         </SwiperSlide>
+         </Swiper>
+
+
+         </div>         </div>
+
     );
 }
 
