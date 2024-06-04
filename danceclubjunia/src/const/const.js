98226df5e12a60d15b21a133ec63481aadcf258e
HugoMany <90179566+HugoMany@users.noreply.github.com>
2024-06-04 10:33:23 +0200

    Variablisation

---
+++ danceclubjunia/src/const/const.js
@@ -0,0 +1,19 @@
+
+// constants.js
+
+export const URL_DB = 'Some Value';
+
+
+
+
+
+
+
+
+// Ex  d'integration
+
+// SomeOtherFile.js
+
+// import { MY_CONSTANT, ANOTHER_CONSTANT, YET_ANOTHER_CONSTANT } from './constants';
+
+// console.log(MY_CONSTANT); // Affiche : 'Some Value'
