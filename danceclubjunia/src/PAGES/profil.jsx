7b060fca0ab4f714e3196f26da2d8f741c7f9530
HugoMany <90179566+HugoMany@users.noreply.github.com>
2024-06-05 13:52:21 +0200

    profil historique

--- danceclubjunia/src/PAGES/profil.jsx
+++ danceclubjunia/src/PAGES/profil.jsx
@@ -22,6 +22,7 @@ showLoading();
 
 const Profil = () => {
     const [userData, setUserData] = useState(null);
+    const [userPaymentHistory, setPaymentHistory] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -46,10 +47,34 @@ const Profil = () => {
                 hideLoading();
             }
         };
+        const fetchPaymentHistory = async () => {
+            try {
+                const response = await fetch(URL_DB + 'student/getPaymentHistory?studentID=2', {
+                    method: 'GET',
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    setPaymentHistory(data);
+                    setLoading(false);
+                    console.log(data);
+                } else {
+                    console.error('Erreur lors de la récupération des prof');
+                }
+            } catch (error) {
+                console.error('Erreur lors de la récupération des prof', error);
+            }
+            finally {
+                hideLoading();
+            }
+        };
 
         fetchUser();
-
+        fetchPaymentHistory();
+        
     }, []);
+
+    
 if (loading) {
         return <Loading></Loading>;
       }
@@ -57,14 +82,47 @@ if (loading) {
     <div className='Profil'>
       <Header title="Profil"></Header>
 
-    
+
       <h2>Vos informations</h2>
       <p>Firstname: {userData?.student.firstname}</p>
       <p>Surname: {userData?.student.surname}</p>
       <p>Email: {userData?.student.email}</p>
       <p>Credit: {userData?.student.credit}</p>
+    
 
+    <h2>Historique d'achat</h2>
+    
+    {/* <div>{userPaymentHistory?.payments[0].paymentID}</div>
+    <div>{userPaymentHistory?.payments[0].price}</div>
+    <div>{userPaymentHistory?.payments[0].type}</div>
+    <div>{userPaymentHistory?.payments[0].quantity}</div> */}
+    <div className='paiementList'>
+    {userPaymentHistory?.payments.map((payment, index) => (
+        <div key={index}>
+            <div>Payment ID: {payment.paymentID}</div>
+            <div>Price: {payment.price}</div>
+            <div>Type: {payment.type}</div>
+            <div>Quantity: {payment.quantity}</div>
+            <br></br>
+        </div>
+    ))}
+</div>
+
+    <div>
       <h2>Vos anciens cours</h2>
+      <div>
+        
+            {/* <p>Payment ID: {payment.paymentID}</p>
+            <p>User ID: {payment.userID}</p>
+            <p>Price: {payment.price}</p>
+            <p>Type: {payment.type}</p>
+            <p>Quantity: {payment.quantity}</p>
+            <p>Date: {payment.date}</p>
+            <p>Payment Type: {payment.paymentType}</p>
+          </div> */}
+
+      </div>
+    </div>  
 
       <div>
         <PastCoursesEleve></PastCoursesEleve>
